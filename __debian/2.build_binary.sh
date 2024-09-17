5f626183a8940978057508ccfca7bd8c81fdcf95
jeanfrancoisgratton <jean-francois@famillegratton.net>
2024-09-17 12:58:53 -0400

    Added systemd service unit to debian packaging

--- __debian/2.build_binary.sh
+++ __debian/2.build_binary.sh
@@ -2,8 +2,7 @@
 
 PKGDIR="prometheusListener-1.02.00-0_amd64"
 
-mkdir -p ${PKGDIR}/opt/bin ${PKGDIR}/DEBIAN
-mkdir -p ${PKGDIR}/opt/bin ${PKGDIR}/DEBIAN
+mkdir -p ${PKGDIR}/opt/sbin ${PKGDIR}/DEBIAN ${PKGDIR}/etc/systemd/system
 for i in control preinst prerm postinst postrm;do
   mv $i ${PKGDIR}/DEBIAN/
 done
@@ -16,4 +15,6 @@ sudo chown 0:0 ../__debian/${PKGDIR}/opt/sbin/prometheusSDlistener
 
 echo "Binary built. Now packaging..."
 cd ../__debian/
+cp ../prometheusSDlistener.service ${PKGDIR}/etc/systemd/system/prometheusSDlistener.service
+chmod 644 ${PKGDIR}/etc/systemd/system/prometheusSDlistener.service
 dpkg-deb -b ${PKGDIR}
